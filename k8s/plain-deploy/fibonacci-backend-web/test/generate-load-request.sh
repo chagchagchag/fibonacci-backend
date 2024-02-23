47ff3a3990e59a76d9082d0a85d3968e073bb16a
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-23 11:08:10 +0900

    테스트 문서 및 스샷 작성 전 백업

--- k8s/plain-deploy/fibonacci-backend-web/test/generate-load-request.sh
+++ k8s/plain-deploy/fibonacci-backend-web/test/generate-load-request.sh
@@ -4,7 +4,7 @@ while true; do
   count=50
 
   for ((i = 0; i < count; i++)); do
-    curl "http://localhost:8080/fibonacci?number=1" &
+    curl "http://localhost/fibonacci?number=1" &
   done
 
   sleep 1
