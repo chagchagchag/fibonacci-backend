51819d26ca1e81f76af204ae84dd47598de69085
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-24 16:09:22 +0900

    hpa 테스트 용도의 리소스 정의파일 + 테스트 쉘스크립트

--- k8s/plain-deploy/fibonacci-backend-web/test/generate-load-request.sh
+++ k8s/plain-deploy/fibonacci-backend-web/test/generate-load-request.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 while true; do
-  count=50
+  # count=100
+  count=200
 
   for ((i = 0; i < count; i++)); do
-    curl "http://localhost/fibonacci?number=1" &
+    curl "http://localhost/fibonacci?number=11" &
   done
 
-  sleep 1
+  sleep 0.5
 done
